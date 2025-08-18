2e16bb62fad703f945ce79b4c045614b6e076e13
haqimiM9 <it.merchant9@outlook.com>
2025-08-18 23:25:05 +0800

    update: login

--- resources/js/layouts/auth/AuthSplitLayout.vue
+++ resources/js/layouts/auth/AuthSplitLayout.vue
@@ -13,12 +13,13 @@ defineProps<{
 </script>
 
 <template>
-    <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div
+        class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div class="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
             <div class="absolute inset-0 bg-zinc-900" />
             <Link :href="route('dashboard')" class="relative z-20 flex items-center text-lg font-medium">
-                <AppLogoIcon class="mr-2 size-8 fill-current text-white" />
-                {{ name }}
+            <AppLogoIcon class="mr-2 size-8 fill-current text-white" />
+            {{ name }}
             </Link>
             <div v-if="quote" class="relative z-20 mt-auto">
                 <blockquote class="space-y-2">
@@ -29,6 +30,16 @@ defineProps<{
         </div>
         <div class="lg:p-8">
             <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                <div class="flex justify-center gap-2 md:justify-start block md:hidden">
+                    <a :href="route('user_dashboard')" class="flex items-center gap-2 font-medium">
+                        <div
+                            class="flex h-8 w-8 items-center justify-center rounded-md bg-primary text-primary-foreground">
+                            <!-- <GalleryVerticalEnd class-name="size-4" /> -->
+                            <AppLogoIcon class="size-6 fill-current" />
+                        </div>
+                         {{ name }}
+                    </a>
+                </div>
                 <div class="flex flex-col space-y-2 text-center">
                     <h1 class="text-xl font-medium tracking-tight" v-if="title">{{ title }}</h1>
                     <p class="text-sm text-muted-foreground" v-if="description">{{ description }}</p>
